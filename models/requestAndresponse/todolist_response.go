0ad1225aeafd473a15b9aa5f7a4f0b2b970dea46
EchoEdyP <[forsocmedecho@proton.me]>
2023-03-04 06:19:55 +0700

    chore: add created at and update at in entity

--- models/requestAndresponse/todolist_response.go
+++ models/requestAndresponse/todolist_response.go
@@ -1,8 +1,12 @@
 package requestAndresponse
 
+import "time"
+
 type TodoListResponse struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
