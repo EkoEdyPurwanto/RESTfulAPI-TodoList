b39a2ed10bad0aad843a56c37bba57c4ef46bb81
EchoEdyP <forsocmedecho@proton.me>
2023-06-04 10:04:57 +0700

    change: value in user n dbname

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,9 +2,9 @@ package config
 
 type Config struct {
 	DBDriver string `envconfig:"DB_DRIVER" default:"postgres"`
-	DBUser   string `envconfig:"DB_USER" default:"postgres"`
+	DBUser   string `envconfig:"DB_USER" default:"eep"`
 	DBPass   string `envconfig:"DB_PASS" default:"1903"`
-	DBName   string `envconfig:"DB_NAME" default:"RESTfulAPI_todos"`
+	DBName   string `envconfig:"DB_NAME" default:"restfulapi_todos"`
 	DBHost   string `envconfig:"DB_HOST" default:"localhost"`
 	DBPort   int    `envconfig:"DB_PORT" default:"5432"`
 }
