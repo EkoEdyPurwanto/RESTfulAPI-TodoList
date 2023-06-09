f6a1500fb2e473fbbb24a63f8db642f25f31f2a2
EchoEdyP <forsocmedecho@proton.me>
2023-06-09 22:27:11 +0700

    change: dbName to db container

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,6 @@ type Config struct {
 	DBUser   string `envconfig:"DB_USER" default:"eep"`
 	DBPass   string `envconfig:"DB_PASS" default:"1903"`
 	DBName   string `envconfig:"DB_NAME" default:"restfulapi_todos"`
-	DBHost   string `envconfig:"DB_HOST" default:"localhost"`
+	DBHost   string `envconfig:"DB_HOST" default:"psql-mycontainer"`
 	DBPort   int    `envconfig:"DB_PORT" default:"5432"`
 }
