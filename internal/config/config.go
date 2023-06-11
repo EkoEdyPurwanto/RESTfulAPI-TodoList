d229b5291dbe24737e3dabf0ceb84ff7ff081983
EchoEdyP <forsocmedecho@proton.me>
2023-06-11 13:12:23 +0700

    change value: adUser and dbHost

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,9 +2,9 @@ package config
 
 type Config struct {
 	DBDriver string `envconfig:"DB_DRIVER" default:"postgres"`
-	DBUser   string `envconfig:"DB_USER" default:"eep"`
+	DBUser   string `envconfig:"DB_USER" default:"eepsql"`
 	DBPass   string `envconfig:"DB_PASS" default:"1903"`
 	DBName   string `envconfig:"DB_NAME" default:"restfulapi_todos"`
-	DBHost   string `envconfig:"DB_HOST" default:"psql-mycontainer"`
+	DBHost   string `envconfig:"DB_HOST" default:"localhost"`
 	DBPort   int    `envconfig:"DB_PORT" default:"5432"`
 }
